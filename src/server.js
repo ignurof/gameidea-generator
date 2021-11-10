a34eef3f465a4271678feb83706cc571f4346dca
ignurof <ignurof@outlook.com>
2021-11-10 20:33:55 +0100

    setup required svelte and additional config file, also add global.css for future reference

--- src/server.js
+++ src/server.js
@@ -1,1 +1,30 @@
-console.log("Hello world");
+const express = require("express");
+const svelteViewEngine = require("svelte-view-engine");
+const config = require("../config.js");
+
+let app = express();
+const PORT = 3000;
+
+// View Engine declarations
+let engine = svelteViewEngine(config.svelteViewEngine);
+let { dir, type, buildDir } = config.svelteViewEngine;
+
+// View Engine setup
+app.engine(type, engine.render);
+app.set("view engine", type);
+app.set("views", dir);
+
+// Change this to fit your static content (Images, etc.)
+app.use("/assets", express.static(buildDir));
+
+// Render the response with props before responding
+app.get("/", (req, res, next) => {
+    res.render("index", {
+        // Props here
+        
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("Server listening on port: " + PORT);
+});
